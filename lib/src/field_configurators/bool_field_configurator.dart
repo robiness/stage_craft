50a45371ba7821fd78d6d5c33348b941ad107607
Dev-dfm <d.floresmedina@gmail.com>
2023-03-07 18:03:05 +0100

    Add BoolFieldConfigurator

---
+++ lib/src/field_configurators/bool_field_configurator.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:widget_stage/widget_stage.dart';
+
+class BoolFieldConfigurator extends FieldConfigurator<bool> {
+  BoolFieldConfigurator({
+    required super.value,
+    required super.name,
+  });
+
+  @override
+  Widget builder(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Expanded(child: Text('$name: ')),
+        Expanded(
+          child: Switch(
+            value: value,
+            activeColor: Colors.green,
+            onChanged: (bool newValue) {
+              value = newValue;
+              notifyListeners();
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
