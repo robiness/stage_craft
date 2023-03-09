ea76ec465b821d28342da3a4c1accc507fe5ada1
robiness <robin.schonau@gmail.com>
2023-03-09 12:16:27 +0100

    Add grid

--- lib/src/flexible_stage.dart
+++ lib/src/flexible_stage.dart
@@ -101,174 +101,177 @@ class _ResizableWidgetState extends State<FlexibleStage> {
     return LayoutBuilder(
       builder: (context, constraints) {
         currentConstraints = constraints;
-        return Stack(
-          children: <Widget>[
-            // The actual widget
-            Positioned(
-              top: top,
-              left: left,
-              child: Padding(
-                padding: const EdgeInsets.all(handlePadding),
-                child: GestureDetector(
-                  onPanUpdate: (details) {
-                    final dx = details.delta.dx;
-                    final dy = details.delta.dy;
-                    final newWidth = width + dx;
-                    final newHeight = height + dy;
-                    if (left + dx >= 0 &&
-                        (newWidth + left) <= (currentConstraints!.maxWidth - 70) &&
-                        top + dy >= 0 &&
-                        (newHeight + top) <= (currentConstraints!.maxHeight - 70)) {
-                      setState(() {
-                        top += details.delta.dy;
-                        left += details.delta.dx;
-                      });
-                    }
-                  },
-                  child: Container(
-                    height: height,
-                    width: width,
-                    color: Colors.black26,
-                    child: widget.child,
-                  ),
-                ),
-              ),
-            ),
-            // top left
-            Positioned(
-              top: top - ballDiameter / 2 + handlePadding,
-              left: left - ballDiameter / 2 + handlePadding,
-              child: ManipulatingBall(
-                show: showManipulatingBalls,
-                onDragStart: onDragStart,
-                onDragEnd: onDragEnd,
-                onDrag: (dx, dy) {
-                  dragUp(dx, dy);
-                  dragLeft(dx, dy);
-                },
-              ),
-            ),
-            // top right
-            Positioned(
-              top: top - ballDiameter / 2 + handlePadding,
-              left: left + width - ballDiameter / 2 + handlePadding,
-              child: ManipulatingBall(
-                show: showManipulatingBalls,
-                onDragStart: onDragStart,
-                onDragEnd: onDragEnd,
-                onDrag: (dx, dy) {
-                  dragRight(dx, dy);
-                  dragUp(dx, dy);
-                },
-              ),
-            ),
-
-            // bottom right
-            Positioned(
-              top: top + height - ballDiameter / 2 + handlePadding,
-              left: left + width - ballDiameter / 2 + handlePadding,
-              child: ManipulatingBall(
-                show: showManipulatingBalls,
-                onDragStart: onDragStart,
-                onDragEnd: onDragEnd,
-                onDrag: (dx, dy) {
-                  dragDown(dx, dy);
-                  dragRight(dx, dy);
-                },
-              ),
-            ),
-            // top middle
-            Positioned(
-              top: top - ballDiameter / 2 + handlePadding,
-              left: left + width / 2 - ballDiameter / 2 + handlePadding,
-              child: ManipulatingBall(
-                show: showManipulatingBalls,
-                onDragStart: onDragStart,
-                onDragEnd: onDragEnd,
-                onDrag: dragUp,
-              ),
-            ),
-            // bottom center
-            Positioned(
-              top: top + height - ballDiameter / 2 + handlePadding,
-              left: left + width / 2 - ballDiameter / 2 + handlePadding,
-              child: ManipulatingBall(
-                show: showManipulatingBalls,
-                onDragStart: onDragStart,
-                onDragEnd: onDragEnd,
-                onDrag: dragDown,
-              ),
-            ),
-            // bottom left
-            Positioned(
-              top: top + height - ballDiameter / 2 + handlePadding + 24,
-              left: left + handlePadding,
-              child: Text('${width.toStringAsFixed(1)} x ${height.toStringAsFixed(1)}'),
-            ),
-            // bottom left
-            Positioned(
-              top: top + height - ballDiameter / 2 + handlePadding,
-              left: left - ballDiameter / 2 + handlePadding,
-              child: ManipulatingBall(
-                show: showManipulatingBalls,
-                onDragStart: onDragStart,
-                onDragEnd: onDragEnd,
-                onDrag: (dx, dy) {
-                  dragLeft(dx, dy);
-                  dragDown(dx, dy);
-                },
-              ),
-            ),
-            //left center
-            Positioned(
-              top: top + height / 2 - ballDiameter / 2 + handlePadding,
-              left: left - ballDiameter / 2 + handlePadding,
-              child: ManipulatingBall(
-                show: showManipulatingBalls,
-                onDragStart: onDragStart,
-                onDragEnd: onDragEnd,
-                onDrag: dragLeft,
-              ),
-            ),
-            // center right
-            Positioned(
-              top: top + height / 2 - ballDiameter / 2 + handlePadding,
-              left: left + width - ballDiameter / 2 + handlePadding,
-              child: ManipulatingBall(
-                show: showManipulatingBalls,
-                onDragStart: onDragStart,
-                onDragEnd: onDragEnd,
-                onDrag: dragRight,
-              ),
-            ),
-            // Just here to detect mouse enter and exit around stage
-            Positioned(
-              top: top,
-              left: left,
-              child: MouseRegion(
-                hitTestBehavior: HitTestBehavior.translucent,
-                onEnter: (_) {
-                  setState(() {
-                    showManipulatingBalls = true;
-                  });
-                },
-                onExit: (_) {
-                  setState(() {
-                    if (_isDragging == false) {
-                      showManipulatingBalls = false;
-                    }
-                  });
-                },
+        return CustomPaint(
+          painter: GridRaster(),
+          child: Stack(
+            children: <Widget>[
+              // The actual widget
+              Positioned(
+                top: top,
+                left: left,
                 child: Padding(
                   padding: const EdgeInsets.all(handlePadding),
-                  child: SizedBox(
-                    height: height,
-                    width: width,
+                  child: GestureDetector(
+                    onPanUpdate: (details) {
+                      final dx = details.delta.dx;
+                      final dy = details.delta.dy;
+                      final newWidth = width + dx;
+                      final newHeight = height + dy;
+                      if (left + dx >= 0 &&
+                          (newWidth + left) <= (currentConstraints!.maxWidth - 70) &&
+                          top + dy >= 0 &&
+                          (newHeight + top) <= (currentConstraints!.maxHeight - 70)) {
+                        setState(() {
+                          top += details.delta.dy;
+                          left += details.delta.dx;
+                        });
+                      }
+                    },
+                    child: Container(
+                      height: height,
+                      width: width,
+                      color: Colors.black26,
+                      child: widget.child,
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+              // top left
+              Positioned(
+                top: top - ballDiameter / 2 + handlePadding,
+                left: left - ballDiameter / 2 + handlePadding,
+                child: ManipulatingBall(
+                  show: showManipulatingBalls,
+                  onDragStart: onDragStart,
+                  onDragEnd: onDragEnd,
+                  onDrag: (dx, dy) {
+                    dragUp(dx, dy);
+                    dragLeft(dx, dy);
+                  },
+                ),
+              ),
+              // top right
+              Positioned(
+                top: top - ballDiameter / 2 + handlePadding,
+                left: left + width - ballDiameter / 2 + handlePadding,
+                child: ManipulatingBall(
+                  show: showManipulatingBalls,
+                  onDragStart: onDragStart,
+                  onDragEnd: onDragEnd,
+                  onDrag: (dx, dy) {
+                    dragRight(dx, dy);
+                    dragUp(dx, dy);
+                  },
+                ),
+              ),
+
+              // bottom right
+              Positioned(
+                top: top + height - ballDiameter / 2 + handlePadding,
+                left: left + width - ballDiameter / 2 + handlePadding,
+                child: ManipulatingBall(
+                  show: showManipulatingBalls,
+                  onDragStart: onDragStart,
+                  onDragEnd: onDragEnd,
+                  onDrag: (dx, dy) {
+                    dragDown(dx, dy);
+                    dragRight(dx, dy);
+                  },
+                ),
+              ),
+              // top middle
+              Positioned(
+                top: top - ballDiameter / 2 + handlePadding,
+                left: left + width / 2 - ballDiameter / 2 + handlePadding,
+                child: ManipulatingBall(
+                  show: showManipulatingBalls,
+                  onDragStart: onDragStart,
+                  onDragEnd: onDragEnd,
+                  onDrag: dragUp,
+                ),
+              ),
+              // bottom center
+              Positioned(
+                top: top + height - ballDiameter / 2 + handlePadding,
+                left: left + width / 2 - ballDiameter / 2 + handlePadding,
+                child: ManipulatingBall(
+                  show: showManipulatingBalls,
+                  onDragStart: onDragStart,
+                  onDragEnd: onDragEnd,
+                  onDrag: dragDown,
+                ),
+              ),
+              // bottom left
+              Positioned(
+                top: top + height - ballDiameter / 2 + handlePadding + 24,
+                left: left + handlePadding,
+                child: Text('${width.toStringAsFixed(1)} x ${height.toStringAsFixed(1)}'),
+              ),
+              // bottom left
+              Positioned(
+                top: top + height - ballDiameter / 2 + handlePadding,
+                left: left - ballDiameter / 2 + handlePadding,
+                child: ManipulatingBall(
+                  show: showManipulatingBalls,
+                  onDragStart: onDragStart,
+                  onDragEnd: onDragEnd,
+                  onDrag: (dx, dy) {
+                    dragLeft(dx, dy);
+                    dragDown(dx, dy);
+                  },
+                ),
+              ),
+              //left center
+              Positioned(
+                top: top + height / 2 - ballDiameter / 2 + handlePadding,
+                left: left - ballDiameter / 2 + handlePadding,
+                child: ManipulatingBall(
+                  show: showManipulatingBalls,
+                  onDragStart: onDragStart,
+                  onDragEnd: onDragEnd,
+                  onDrag: dragLeft,
+                ),
+              ),
+              // center right
+              Positioned(
+                top: top + height / 2 - ballDiameter / 2 + handlePadding,
+                left: left + width - ballDiameter / 2 + handlePadding,
+                child: ManipulatingBall(
+                  show: showManipulatingBalls,
+                  onDragStart: onDragStart,
+                  onDragEnd: onDragEnd,
+                  onDrag: dragRight,
+                ),
+              ),
+              // Just here to detect mouse enter and exit around stage
+              Positioned(
+                top: top,
+                left: left,
+                child: MouseRegion(
+                  hitTestBehavior: HitTestBehavior.translucent,
+                  onEnter: (_) {
+                    setState(() {
+                      showManipulatingBalls = true;
+                    });
+                  },
+                  onExit: (_) {
+                    setState(() {
+                      if (_isDragging == false) {
+                        showManipulatingBalls = false;
+                      }
+                    });
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(handlePadding),
+                    child: SizedBox(
+                      height: height,
+                      width: width,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         );
       },
     );
@@ -345,5 +348,31 @@ class _ManipulatingBallState extends State<ManipulatingBall> {
         ),
       ),
     );
+  }
+}
+
+class GridRaster extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    for (var i = 0; i < size.width; i += 100) {
+      canvas.drawLine(
+        Offset(i.toDouble(), 0),
+        Offset(i.toDouble(), size.height),
+        Paint()..color = Colors.grey.withOpacity(0.1),
+      );
+    }
+    // draw vertical lines
+    for (var i = 0; i < size.height; i += 100) {
+      canvas.drawLine(
+        Offset(0, i.toDouble()),
+        Offset(size.width, i.toDouble()),
+        Paint()..color = Colors.grey.withOpacity(0.1),
+      );
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
