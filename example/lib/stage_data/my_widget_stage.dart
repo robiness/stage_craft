7874d875e5e2deab92ea687fb550eeeb435a2f72
robiness <robin.schonau@gmail.com>
2023-08-10 13:11:03 +0200

    Adjust first example widget

--- example/lib/stage_data/my_widget_stage.dart
+++ example/lib/stage_data/my_widget_stage.dart
@@ -6,9 +6,11 @@ import 'package:stage_craft/stage_craft.dart';
 class MyWidgetStageData extends WidgetStageData {
   MyWidgetStageData()
       : _color = ColorFieldConfiguratorNullable(value: null, name: 'color'),
+        _borderColor = ColorFieldConfiguratorNullable(value: null, name: 'borderColor'),
         _text = StringFieldConfigurator(value: "My text", name: 'text'),
         _nullableBool = BoolFieldConfiguratorNullable(value: false, name: 'nullableBool'),
-        _borderRadius = DoubleFieldConfiguratorNullable(value: 4, name: 'borderRadius');
+        _borderRadius = DoubleFieldConfiguratorNullable(value: 4, name: 'borderRadius'),
+        _padding = PaddingFieldConfigurator(value: const EdgeInsets.all(8), name: 'padding');
 
   @override
   String get name => 'MyWidget';
@@ -17,17 +19,21 @@ class MyWidgetStageData extends WidgetStageData {
   Size get stageSize => const Size(200, 200);
 
   final ColorFieldConfiguratorNullable _color;
+  final ColorFieldConfiguratorNullable _borderColor;
   final StringFieldConfigurator _text;
   final BoolFieldConfiguratorNullable _nullableBool;
   final DoubleFieldConfiguratorNullable _borderRadius;
+  final PaddingFieldConfigurator _padding;
 
   @override
   Widget widgetBuilder(BuildContext context) {
     return MyWidget(
       color: _color.value,
+      borderColor: _borderColor.value,
       text: _text.value,
       borderRadius: _borderRadius.value,
       isTrue: _nullableBool.value,
+      padding: _padding.value,
     );
   }
 
@@ -35,9 +41,11 @@ class MyWidgetStageData extends WidgetStageData {
   List<FieldConfigurator> get widgetConfigurators {
     return [
       _color,
+      _borderColor,
       _text,
       _borderRadius,
       _nullableBool,
+      _padding,
     ];
   }
 
--- example/lib/widgets/my_widget.dart
+++ example/lib/widgets/my_widget.dart
@@ -6,29 +6,54 @@ class MyWidget extends StatelessWidget {
     required this.color,
     required this.text,
     this.isTrue,
+    this.borderColor,
+    EdgeInsets? padding,
     this.borderRadius = 8,
-  });
+  }) : padding = padding ?? const EdgeInsets.all(8.0);
 
   final Color? color;
+  final Color? borderColor;
   final double? borderRadius;
   final String text;
   final bool? isTrue;
+  final EdgeInsets padding;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: color ?? Colors.transparent,
-        borderRadius: BorderRadius.circular(borderRadius ?? 0),
-      ),
+    return Padding(
+      padding: padding,
       child: Container(
         decoration: BoxDecoration(
-          border: Border.all(),
+          color: color ?? Colors.transparent,
           borderRadius: BorderRadius.circular(borderRadius ?? 0),
         ),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text("$text is $isTrue"),
+        child: Row(
+          children: [
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: borderColor ?? const Color(0xFF000000)),
+                  borderRadius: BorderRadius.circular(borderRadius ?? 0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text("$text is $isTrue"),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: borderColor ?? const Color(0xFF000000)),
+                  borderRadius: BorderRadius.circular(borderRadius ?? 0),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text("right"),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
