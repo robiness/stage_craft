00bd18a6622e560d46ff5d0fe994054d2a267252
robiness <robin.schonau@gmail.com>
2024-06-21 15:06:24 +0200

    Update dependencies

--- example/lib/new/new_example.dart
+++ example/lib/new/new_example.dart
@@ -30,15 +30,11 @@ class FunkyContainer extends StatefulWidget {
   const FunkyContainer({
     super.key,
     this.color,
-    this.height,
-    this.width,
     this.child,
     this.controller,
   });
 
   final Color? color;
-  final double? height;
-  final double? width;
   final Widget? child;
   final TextEditingController? controller;
 
@@ -47,37 +43,21 @@ class FunkyContainer extends StatefulWidget {
 }
 
 class _FunkyContainerState extends State<FunkyContainer> {
-  double _width = 100;
-
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
         print('constraints: $constraints');
         return Center(
-          child: GestureDetector(
-            onTap: _onFunkyTap,
-            child: Container(
-              height: widget.height,
-              width: widget.width,
-              decoration: BoxDecoration(
-                color: widget.color,
-                borderRadius: BorderRadius.circular(24),
-              ),
-              child: widget.child,
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              color: widget.color,
+              borderRadius: BorderRadius.circular(24),
             ),
+            child: widget.child,
           ),
         );
       },
     );
-  }
-
-  void _onFunkyTap() {
-    if (_width == 200) {
-      _width = 90;
-    }
-    setState(() {
-      _width += 10;
-    });
   }
 }
