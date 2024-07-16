28ad9ad76fc639ba07467138992705bb00e1d594
robiness <robin.schonau@gmail.com>
2024-07-16 11:06:51 +0200

    Remove window manager

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,24 +1,7 @@
 import 'package:example/my_awesome_widget_stage.dart';
 import 'package:flutter/material.dart';
-import 'package:window_manager/window_manager.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  // Must add this line.
-  await windowManager.ensureInitialized();
-
-  const WindowOptions windowOptions = WindowOptions(
-    size: Size(1200, 800),
-    center: true,
-    backgroundColor: Colors.transparent,
-    skipTaskbar: false,
-    titleBarStyle: TitleBarStyle.hidden,
-  );
-
-  windowManager.waitUntilReadyToShow(windowOptions, () async {
-    await windowManager.show();
-    await windowManager.focus();
-  });
   runApp(
     const MaterialApp(
       debugShowCheckedModeBanner: false,
