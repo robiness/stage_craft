001b092d873771eb31191a3b795cd84fcd9228f5
robiness <robin.schonau@gmail.com>
2024-10-22 08:53:16 +0200

    Export DefaultControlBarRow

--- lib/stage_craft.dart
+++ lib/stage_craft.dart
@@ -1,4 +1,5 @@
 export 'src/controls/controls.dart';
 export 'src/stage/stage.dart';
 export 'src/stage/stage_style.dart';
+export 'src/widgets/default_control_bar_row.dart';
 export 'src/widgets/stage_craft_color_picker.dart';
